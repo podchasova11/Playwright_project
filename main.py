c855b82323bd3e0c56feacd2eb26355a435ae59a
Mila Podchasova <Pod4asova20@gmail.com>
2024-06-15 12:20:50 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+import re
+from playwright.sync_api import Playwright, sync_playwright, expect
+
+
+def run(playwright: Playwright) -> None:
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context()
+    page = context.new_page()
+    page.goto("https://demo.playwright.dev/todomvc/#/")
+    page.get_by_placeholder("What needs to be done?").fill("Создать первый сценарий плайрайт")
+    page.get_by_placeholder("What needs to be done?").press("Enter")
+    page.get_by_role("link", name="Completed").click()
+
+    # ---------------------
+    context.close()
+    browser.close()
+
+
+with sync_playwright() as playwright:
+    run(playwright)
